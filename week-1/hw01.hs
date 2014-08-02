b435c5adb825d8abb43b6cc62507f2e08cde41cc
Karl Sutt <karl@sutt.ee>
2014-08-02 17:10:32 +0100

    (concat $ map) is concatMap

--- week-1/hw01.hs
+++ week-1/hw01.hs
@@ -15,14 +15,14 @@ doubleEveryOther xs = reverse $ double xs
   where double = zipWith ($) (cycle [id, (*2)])
 
 sumDigits :: [Integer] -> Integer
-sumDigits xs = sum $ concat $ map toDigits xs
+sumDigits xs = sum $ concatMap toDigits xs
 
 validate :: Integer -> Bool
 validate n = checkSum `mod` 10 == 0
   where checkSum = (sumDigits . doubleEveryOther . toDigitsRev) n
 
 
--- Excercises 5-6
+-- Excercise 5
 type Peg  = String
 type Move = (Peg, Peg)
 
